43131b82660a517e06824c242706a15e7b83ba90
Craig <panamaworldwideinvestments@gmail.com>
2022-07-28 13:13:40 +0100

    added seeds file

---
+++ db/createSeeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (department_name)
+VALUES ("Bricolaje"), 
+("Carpinteria"), 
+("Electricidad"), 
+("Pintura"), 
+("Plomeria"), 
+("Cerrajeria"), 
+       
+
+  
+INSERT INTO employee_role (title, salary, department_id)
+VALUES ("dependiente", 18.000,001),
+("carpintero", 20.000,002),
+("electricista", 52.000,003),
+("pintor", 24.000,004),
+("plomero", 26.000,005),
+("cerrajero", 28.000,006),
+
+
+       
+
+INSERT INTO employee (first_name, last_name, employee_role_id, manager_id)     
+VALUES ("Jorge", "Gonzalez", 1, NULL),
+       ("Teofimo", "Lopez", 2, 1),
+       ("Kafu", "Bantan", 3, 1),
+       ("Frankie","Paul",4, 1),
+       ("Jimmy", "JZorrilo",5, 1),
+       ("Monica", "Garcia",6 ,1)
+
+    
+
