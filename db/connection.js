e01ee338d4792caf8ef92a7aaeb8e142c6a13d6d
Craig <panamaworldwideinvestments@gmail.com>
2022-08-07 19:37:22 +0100

    I give up

--- db/connection.js
+++ db/connection.js
@@ -6,11 +6,12 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
 
-    port:  process.env.PORT || 3000,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  
+    
+        host: 'localhost',
+        user: 'root',
+        password: 'Hellobye1',
+        database: 'employee_db'
     
 
 });
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const connection = require("./db/connection");
 
 
-questions();
+
 
 function questions(){
 
