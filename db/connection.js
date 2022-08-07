7938320505501dbff02f143680f1e4bd422edce8
Craig <panamaworldwideinvestments@gmail.com>
2022-08-07 18:50:28 +0100

    changed capitalization in connection.js to match with .env file

--- db/connection.js
+++ db/connection.js
@@ -6,11 +6,13 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
 
-  host: process.env.host,
-  port: process.env.port,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
+    port:  process.env.PORT || 3000,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+    
+
 });
 
 connection.connect(function (err) {
